bb19be6b03b56dbd71fc3d3d3cdc1f65eb47fa22
Prince Yadav <167596785+warrior-hub@users.noreply.github.com>
2026-09-21 00:47:51 +0530

    Change API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://chat-application-t21p.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
